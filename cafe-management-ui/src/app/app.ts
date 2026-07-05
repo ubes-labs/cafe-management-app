b9ab2fc275f0f00b81715a3ca1729de52a96b200
akashneelGhoshDev <akashneel.ghosh.enterprises@gmail.com>
2026-07-05 16:24:10 +0530

    feat: implement layout component and localization for Cafe Management System

--- cafe-management-ui/src/app/app.ts
+++ cafe-management-ui/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Layout } from './layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styles: [],
+  imports: [Layout],
 })
 export class App {}
---
+++ cafe-management-ui/src/app/layout/components/layout/layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-layout',
+  imports: [MatToolbarModule],
+  templateUrl: './layout.html',
+  styles: ``,
+})
+export class Layout {}
